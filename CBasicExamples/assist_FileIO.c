95158d5ad8fb475f0a53655ac93b18c0df7114a7
GUO Yu <111267796+ADMINGUOYU@users.noreply.github.com>
2025-09-17 07:22:46 +0000

    Bug fix (C include issue)

---
+++ CBasicExamples/assist_FileIO.c
@@ -0,0 +1,40 @@
+#include "assist_FileIO.h"
+
+typedef struct fileIO {
+	FILE* fptr;
+	char filePath[MAX_NAME_LENGTH];
+}fileIO;
+
+void fileIO_init(fileIO* file,char* path) {
+	file->fptr = NULL;
+	strncpy(file->filePath, path, MAX_NAME_LENGTH);
+	return;
+}
+
+int fileOpen(fileIO* file,char* mode) {
+	file->fptr = fopen(file->filePath, mode);
+	if (file->fptr == NULL) {
+		printf("fileIO ERROR: failed to open file.\n");
+		return -1;
+	}
+	return 0;
+}
+
+int fileClose(fileIO* file) {
+	if (file->fptr == NULL) {
+		printf("fileIO ERROR: file pointer NULL.\n");
+		return -1;
+	}
+	fclose(file->fptr);
+	file->fptr == NULL;
+	return 0;
+}
+
+int fileWrite(fileIO* file, char* content) {
+	if (file->fptr == NULL) {
+		printf("fileIO ERROR: file pointer NULL.\n");
+		return -1;
+	}
+	fprintf(file->fptr,"%s",content);
+	return 0;
+}
